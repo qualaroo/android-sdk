2899328978df1e357788a01851ac5e1b4752aaab
Artem Orynko <a.orynko@gmail.com>
2016-09-05 14:21:43 +0300

    feat: add class QualarooWebViewClient

---
+++ androidqualaroosdk-src/src/main/java/qualaroo/com/androidqualaroosdk/src/QualarooWebView.java
@@ -0,0 +1,17 @@
+package qualaroo.com.androidqualaroosdk.src;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Artem Orynko on 05.09.16.
+ * Copyright © 2016 Qualaroo. All rights reserved.
+ */
+
+class QualarooWebView extends WebViewClient {
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+    }
+}
