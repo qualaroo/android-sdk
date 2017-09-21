2bda2a91a53d558db3c42eb0fe8711b75c325aaf
scana <scanarch@gmail.com>
2017-09-21 20:32:41 +0200

    Credentials (api key) unpacking

---
+++ qualaroosdk/src/main/java/com/qualaroo/internal/Credentials.java
@@ -0,0 +1,50 @@
+package com.qualaroo.internal;
+
+import okio.ByteString;
+
+public class Credentials {
+
+    private final String apiKey;
+    private final String apiSecret;
+    private final String siteId;
+
+    public Credentials(String exampleKey) {
+        ByteString byteKey = ByteString.decodeBase64(exampleKey);
+        String key = byteKey.utf8();
+        String[] keyParts = key.split(":");
+        if (keyParts.length != 3) {
+            throw new InvalidCredentialsException();
+        }
+        if (!isNumericOnly(keyParts[0])) {
+            throw new InvalidCredentialsException();
+        }
+        if (!isNumericOnly(keyParts[2])) {
+            throw new InvalidCredentialsException();
+        }
+        this.apiKey = keyParts[0];
+        this.apiSecret = keyParts[1];
+        this.siteId = keyParts[2];
+    }
+
+    public String apiKey() {
+        return apiKey;
+    }
+
+    public String apiSecret() {
+        return apiSecret;
+    }
+
+    public String siteId() {
+        return siteId;
+    }
+
+    private static boolean isNumericOnly(String text) {
+        char[] chars = text.toCharArray();
+        for (char c : chars) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
