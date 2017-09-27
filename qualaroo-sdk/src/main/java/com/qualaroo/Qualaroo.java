51592b74a15a8d60d5199cc9c912dd1866bc07ff
scana <marcin@qualaroo.com>
2017-09-27 16:53:58 +0200

    Stop using Inject class
    -- it was only a temporary helper for injecting dependencies

--- qualaroo-sdk/src/main/java/com/qualaroo/Qualaroo.java
+++ qualaroo-sdk/src/main/java/com/qualaroo/Qualaroo.java
@@ -1,51 +1,81 @@
 package com.qualaroo;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.qualaroo.internal.Credentials;
+import com.qualaroo.internal.SessionInfo;
+import com.qualaroo.internal.SurveyDisplayQualifier;
+import com.qualaroo.internal.TimeMatcher;
+import com.qualaroo.internal.TimeProvider;
+import com.qualaroo.internal.UserInfo;
+import com.qualaroo.internal.UserPropertiesMatcher;
+import com.qualaroo.internal.executor.UiThreadExecutor;
+import com.qualaroo.internal.model.Language;
+import com.qualaroo.internal.model.LanguageJsonDeserializer;
+import com.qualaroo.internal.model.QuestionType;
+import com.qualaroo.internal.model.QuestionTypeDeserializer;
 import com.qualaroo.internal.model.Survey;
-import com.qualaroo.internal.network.Callback;
-import com.qualaroo.internal.network.SurveyClient;
+import com.qualaroo.internal.network.ApiConfig;
+import com.qualaroo.internal.network.RestClient;
+import com.qualaroo.internal.network.SurveysRepository;
+import com.qualaroo.internal.storage.DatabaseLocalStorage;
+import com.qualaroo.internal.storage.LocalStorage;
+import com.qualaroo.internal.storage.Settings;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class Qualaroo implements QualarooBase {
 
     private static QualarooBase INSTANCE;
-    private final SurveyClient surveyClient;
-    private final Context context;
-    private List<Survey> surveys = new ArrayList<>();
-    private SurveysListener surveysListener;
-    private Handler handler = new Handler(Looper.getMainLooper());
 
     public static QualarooBase getInstance() {
         return INSTANCE;
     }
 
+    private SurveyDisplayQualifier surveyDisplayQualifier;
 
     public static Builder with(Context context) {
         return new Builder(context);
     }
 
-    public void init() {
-        surveyClient.fetchSurveys(new Callback<List<Survey>>() {
-            @Override public void onSuccess(List<Survey> result) {
-                surveys.clear();
-                surveys.addAll(result);
-                handler.post(new Runnable() {
-                    @Override public void run() {
-                        surveysListener.onSurveysReady(surveys);
-                    }
-                });
-            }
+    private final Context context;
+    private final SurveysRepository surveysRepository;
+    private final Executor uiExecutor;
+    private final Executor backgroundExecutor;
 
-            @Override public void onFailure(Exception exception) {
-                throw new RuntimeException(exception);
-            }
-        });
+
+    private Qualaroo(Context context, Credentials credentials, boolean debugMode) {
+        this.context = context.getApplicationContext();
+        this.uiExecutor = new UiThreadExecutor();
+        this.backgroundExecutor = Executors.newSingleThreadExecutor();
+        LocalStorage localStorage = new DatabaseLocalStorage(this.context);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("qualaroo_prefs", Context.MODE_PRIVATE);
+        Settings settings = new Settings(sharedPreferences);
+        UserInfo userInfo = new UserInfo(settings, localStorage);
+        UserPropertiesMatcher userPropertiesMatcher = new UserPropertiesMatcher(userInfo);
+        TimeMatcher timeMatcher = new TimeMatcher(new TimeProvider());
+        this.surveyDisplayQualifier = new SurveyDisplayQualifier(localStorage, userPropertiesMatcher, timeMatcher);
+
+        RestClient restClient = buildRestClient(credentials);
+        SessionInfo sessionInfo = new SessionInfo(this.context);
+        surveysRepository = new SurveysRepository(credentials.siteId(), restClient, new ApiConfig(), sessionInfo, userInfo, TimeUnit.HOURS.toMillis(1));
     }
 
     @Override public void showSurvey(@NonNull String alias) {
@@ -75,6 +105,42 @@ public class Qualaroo implements QualarooBase {
     @Override public void setPreferredLanguage(@NonNull String iso2Language) {
 
     }
+
+    private RestClient buildRestClient(Credentials credentials) {
+        Gson gson = buildGson();
+        OkHttpClient okHttpClient = buildOkHttpClient(credentials);
+        return new RestClient(okHttpClient, gson);
+    }
+
+    private OkHttpClient buildOkHttpClient(Credentials credentials) {
+        final HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override public void log(String message) {
+                Log.d("OkHttp", message);
+            }
+        });
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        final String authToken = okhttp3.Credentials.basic(credentials.apiKey(), credentials.apiSecret());
+        return new OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .addInterceptor(new Interceptor() {
+                    @Override public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request().newBuilder()
+                                .header("Authorization", authToken)
+                                .build();
+                        return chain.proceed(request);
+                    }
+                })
+                .build();
+    }
+
+    private Gson buildGson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(Language.class, new LanguageJsonDeserializer())
+                .registerTypeAdapter(QuestionType.class, new QuestionTypeDeserializer())
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create();
+    }
+
     public static class Builder {
         private final Context context;
         private Credentials credentials;
