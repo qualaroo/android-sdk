74842ccf16a9dc6c16ad0e9afef9e6b57cc1f60a
scana <marcin@qualaroo.com>
2017-09-27 16:59:40 +0200

    Introducing SurveyRepository class for acquiring and caching surveys
    -- I know naming sucks a lot:
    -- -- TODO: separate current implementation into two clasess: MemorySurveyRepository and NetworkSurveyRepository
    -- -- TODO: create third class which will fetch data from best available source

---
+++ qualaroo-sdk/src/main/java/com/qualaroo/internal/network/SurveysRepository.java
@@ -0,0 +1,124 @@
+package com.qualaroo.internal.network;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.qualaroo.internal.SessionInfo;
+import com.qualaroo.internal.UserInfo;
+import com.qualaroo.internal.model.Survey;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import okhttp3.HttpUrl;
+
+public class SurveysRepository {
+
+    private final String siteId;
+    private final RestClient restClient;
+    private final ApiConfig apiConfig;
+    private final UserInfo userInfo;
+    private final SessionInfo sessionInfo;
+    private final long staleDataTimeLimitInMillis;
+
+    private final Object lock = new Object();
+
+    private CachedResult cachedResult = new CachedResult(null, 0);
+
+    public SurveysRepository(String siteId, RestClient restClient, ApiConfig apiConfig, SessionInfo sessionInfo, UserInfo userInfo, long staleDataTimeLimitInMillis) {
+        this.siteId = siteId;
+        this.restClient = restClient;
+        this.apiConfig = apiConfig;
+        this.sessionInfo = sessionInfo;
+        this.userInfo = userInfo;
+        this.staleDataTimeLimitInMillis = staleDataTimeLimitInMillis;
+    }
+
+    public @NonNull List<Survey> getSurveys() {
+        synchronized (lock) {
+            if (isInvalid(cachedResult)) {
+                refreshData();
+            } else if (isStale(cachedResult)) {
+                refreshDataAsync();
+            }
+            return returnSafely(cachedResult.surveys);
+        }
+    }
+
+    private void refreshData() {
+        List<Survey> surveys = fetchSurveys();
+        if (surveys != null) {
+            synchronized (lock) {
+                cachedResult = new CachedResult(surveys, System.currentTimeMillis());
+            }
+        }
+    }
+
+    private void refreshDataAsync() {
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(new Runnable() {
+            @Override public void run() {
+                refreshData();
+            }
+        });
+    }
+
+    private @NonNull List<Survey> returnSafely(@Nullable List<Survey> surveys) {
+        if (surveys == null) {
+            return Collections.emptyList();
+        }
+        return Collections.unmodifiableList(surveys);
+    }
+
+    private @Nullable List<Survey> fetchSurveys() {
+        final HttpUrl requestUrl = buildSurveyRequestUrl();
+        Result<Survey[]> result = restClient.get(requestUrl, Survey[].class);
+        if (result.isSuccessful()) {
+            return Arrays.asList(result.getData());
+        } else {
+            return null;
+        }
+    }
+
+    private HttpUrl buildSurveyRequestUrl() {
+        HttpUrl.Builder builder = apiConfig.qualarooApi().newBuilder()
+                .addPathSegment("surveys")
+                .addQueryParameter("site_id", siteId)
+                .addQueryParameter("spec", "1")
+                .addQueryParameter("no_superpack", "1");
+        injectAnalyticsParams(builder);
+        return builder.build();
+    }
+
+    private void injectAnalyticsParams(HttpUrl.Builder httpUrlBuilder) {
+        httpUrlBuilder
+                .addQueryParameter("SDK_version", sessionInfo.sdkVersion())
+                .addQueryParameter("client_app", sessionInfo.appName())
+                .addEncodedQueryParameter("device_type", sessionInfo.deviceType())
+                .addQueryParameter("android_version", sessionInfo.androidVersion())
+                .addQueryParameter("device_ID", userInfo.getDeviceId());
+    }
+
+    private boolean isStale(CachedResult cachedResult) {
+        return System.currentTimeMillis() - cachedResult.timestamp > staleDataTimeLimitInMillis;
+    }
+
+    private boolean isInvalid(CachedResult cachedResult) {
+        return cachedResult.surveys == null;
+    }
+
+    private static final class CachedResult {
+        private final List<Survey> surveys;
+        private final long timestamp;
+
+        CachedResult(List<Survey> surveys, long timestamp) {
+            this.surveys = surveys;
+            this.timestamp = timestamp;
+        }
+    }
+
+
+}
---
+++ qualaroo-sdk/src/test/java/com/qualaroo/internal/network/SurveysRepositoryTest.kt
@@ -0,0 +1,103 @@
+package com.qualaroo.internal.network
+
+import com.nhaarman.mockito_kotlin.*
+import com.qualaroo.internal.SessionInfo
+import com.qualaroo.internal.UserInfo
+import com.qualaroo.internal.model.Survey
+import com.qualaroo.internal.model.TestModels.survey
+import okhttp3.HttpUrl
+import org.junit.Assert.*
+import org.junit.Test
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+@Suppress("MemberVisibilityCanPrivate", "IllegalIdentifier")
+class SurveysRepositoryTest {
+
+    val restClient = mock<RestClient>()
+
+    val apiConfig = mock<ApiConfig> {
+        on { qualarooApi() } doReturn HttpUrl.parse("https://api.qualaroo.com/api/v1.5")
+    }
+    val sessionInfo = mock<SessionInfo> {
+        on { appName() } doReturn "com.qualaroo.test"
+        on { androidVersion() } doReturn "27"
+        on { deviceType() } doReturn "GF3210"
+        on { sdkVersion() } doReturn "1.0.0"
+    }
+    val userInfo = mock<UserInfo> {
+        on { deviceId } doReturn "abcd1"
+    }
+
+    val surveysRepository = SurveysRepository("abc123", restClient, apiConfig, sessionInfo, userInfo, TimeUnit.HOURS.toMillis(1))
+
+    @Test
+    fun `calls proper request`() {
+        resetClientReturns(arrayOf(survey(id = 1), survey(id = 2), survey(id = 3)))
+        surveysRepository.surveys
+
+        val captor = argumentCaptor<HttpUrl>()
+        verify(restClient).get(captor.capture(), eq(Array<Survey>::class.java))
+
+        val url = captor.lastValue
+        assertEquals("https", url.scheme())
+        assertEquals("api.qualaroo.com", url.host())
+        assertEquals("/api/v1.5/surveys", url.encodedPath())
+        assertEquals("abc123", url.queryParameter("site_id"))
+        assertEquals("1", url.queryParameter("spec"))
+        assertEquals("1", url.queryParameter("no_superpack"))
+        assertEquals("1.0.0", url.queryParameter("SDK_version"))
+        assertEquals("27", url.queryParameter("android_version"))
+        assertEquals("GF3210", url.queryParameter("device_type"))
+        assertEquals("abcd1", url.queryParameter("device_ID"))
+        assertEquals("com.qualaroo.test", url.queryParameter("client_app"))
+    }
+
+    @Test
+    fun `returns empty list when no surveys available`() {
+        restClientReturnsError(IOException())
+
+        val surveys = surveysRepository.surveys
+        assertNotNull(surveys)
+        assertEquals(0, surveys.size)
+    }
+
+    @Test
+    fun `returns surveys from API when there were no previously stored surveys`() {
+        resetClientReturns(arrayOf(survey(id = 1), survey(id = 2), survey(id = 3)))
+        val surveys = surveysRepository.surveys
+
+        assertEquals(3, surveys.size)
+        assertTrue(surveys.contains(survey(id = 1)))
+        assertTrue(surveys.contains(survey(id = 2)))
+        assertTrue(surveys.contains(survey(id = 3)))
+    }
+
+    @Test
+    fun `caches results`() {
+        resetClientReturns(arrayOf(survey(id = 1), survey(id = 2), survey(id = 3)))
+
+        var surveys = surveysRepository.surveys
+        assertEquals(3, surveys.size)
+        assertTrue(surveys.contains(survey(id = 1)))
+        assertTrue(surveys.contains(survey(id = 2)))
+        assertTrue(surveys.contains(survey(id = 3)))
+
+        surveys = surveysRepository.surveys
+        assertEquals(3, surveys.size)
+        assertTrue(surveys.contains(survey(id = 1)))
+        assertTrue(surveys.contains(survey(id = 2)))
+        assertTrue(surveys.contains(survey(id = 3)))
+
+        verify(restClient, times(1)).get(any(), eq(Array<Survey>::class.java))
+    }
+
+    private fun resetClientReturns(surveys: Array<Survey>) {
+        whenever(restClient.get(any(), eq(Array<Survey>::class.java))).thenReturn(Result.of(surveys))
+    }
+
+    private fun restClientReturnsError(exception: Exception) {
+        whenever(restClient.get(any(), eq(Array<Survey>::class.java))).thenReturn(Result.error(exception))
+    }
+
+}
