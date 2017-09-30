a48d55a2fa3875e21ae2078d12d33df42b8dc4e3
scana <scanarch@gmail.com>
2017-09-30 22:52:19 +0200

    Demo app for testing surveys
    -- added ability set api key/user properties/survey's alias
    -- ability to clean local storage
    -- helper dialog with current logs with an ability to copy those

--- demo-app/src/main/java/qualaroo/com/QualarooMobileDemo/App.java
+++ demo-app/src/main/java/qualaroo/com/QualarooMobileDemo/App.java
@@ -1,16 +1,29 @@
 package qualaroo.com.QualarooMobileDemo;
 
 import android.app.Application;
+import android.widget.Toast;
 
+import com.jakewharton.processphoenix.ProcessPhoenix;
 import com.qualaroo.Qualaroo;
 
 public class App extends Application {
 
+    private Settings settings;
+
     @Override public void onCreate() {
         super.onCreate();
-        Qualaroo.initializeWith(this)
-                .setApiKey("API_KEY_HERE")
-                .setDebugMode(true)
-                .init();
+        if (ProcessPhoenix.isPhoenixProcess(this)) {
+            return;
+        }
+        settings = new Settings(this);
+        String apiKey = settings.getApiKey();
+        try {
+            Qualaroo.initializeWith(this)
+                    .setApiKey(apiKey)
+                    .setDebugMode(true)
+                    .init();
+        } catch (Exception e) {
+            Toast.makeText(this, "Invalid api key provided!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
