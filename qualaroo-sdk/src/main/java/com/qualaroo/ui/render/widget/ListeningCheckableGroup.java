58406a5f7ed48332220db0d14851c246c61fbed6
scana <marcin@qualaroo.com>
2017-12-20 16:15:27 +0100

    Cleared up custom implementation of RadioGroup

--- qualaroo-sdk/src/main/java/com/qualaroo/ui/render/widget/ListeningCheckableGroup.java
+++ qualaroo-sdk/src/main/java/com/qualaroo/ui/render/widget/ListeningCheckableGroup.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2006 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.qualaroo.ui.render.widget;
 
 import android.content.Context;
@@ -27,11 +11,8 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 
 public class ListeningCheckableGroup extends LinearLayout {
-    // holds the checked id; the selection is empty by default
     private int mCheckedId = -1;
-    // tracks children radio buttons checked state
     private CompoundButton.OnCheckedChangeListener mChildOnCheckedChangeListener;
-    // when true, mOnCheckedChangeListener discards events
     private boolean mProtectFromCheckedChange = false;
     private OnCheckedChangeListener mOnCheckedChangeListener;
     private PassThroughHierarchyChangeListener mPassThroughListener;
@@ -56,15 +37,12 @@ public class ListeningCheckableGroup extends LinearLayout {
 
     @Override
     public void setOnHierarchyChangeListener(OnHierarchyChangeListener listener) {
-        // the user listener is delegated to our pass-through listener
         mPassThroughListener.mOnHierarchyChangeListener = listener;
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-
-        // checks the appropriate radio button as requested in the XML file
         if (mCheckedId != -1) {
             mProtectFromCheckedChange = true;
             setCheckedStateForView(mCheckedId, true);
@@ -90,16 +68,6 @@ public class ListeningCheckableGroup extends LinearLayout {
         super.addView(child, index, params);
     }
 
-    /**
-     * <p>Sets the selection to the radio button whose identifier is passed in
-     * parameter. Using -1 as the selection identifier clears the selection;
-     * such an operation is equivalent to invoking {@link #clearCheck()}.</p>
-     *
-     * @param id the unique id of the radio button to select in this group
-     *
-     * @see #getCheckedId()
-     * @see #clearCheck()
-     */
     public void check(@IdRes int id) {
         // don't even bother
         if (id != -1 && (id == mCheckedId)) {
@@ -131,55 +99,24 @@ public class ListeningCheckableGroup extends LinearLayout {
         }
     }
 
-    /**
-     * <p>Returns the identifier of the selected radio button in this group.
-     * Upon empty selection, the returned value is -1.</p>
-     *
-     * @return the unique id of the selected radio button in this group
-     *
-     * @see #check(int)
-     * @see #clearCheck()
-     *
-     * @attr ref android.R.styleable#RadioGroup_checkedButton
-     */
     @IdRes
     public int getCheckedId() {
         return mCheckedId;
     }
 
-    /**
-     * <p>Clears the selection. When the selection is cleared, no radio button
-     * in this group is selected and {@link #getCheckedId()} returns
-     * null.</p>
-     *
-     * @see #check(int)
-     * @see #getCheckedId()
-     */
     public void clearCheck() {
         check(-1);
     }
 
-    /**
-     * <p>Register a callback to be invoked when the checked radio button
-     * changes in this group.</p>
-     *
-     * @param listener the callback to call on checked state change
-     */
     public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
         mOnCheckedChangeListener = listener;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
         return new ListeningCheckableGroup.LayoutParams(getContext(), attrs);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
         return p instanceof ListeningCheckableGroup.LayoutParams;
@@ -234,18 +171,7 @@ public class ListeningCheckableGroup extends LinearLayout {
         }
     }
 
-    /**
-     * <p>Interface definition for a callback to be invoked when the checked
-     * radio button changed in this group.</p>
-     */
     public interface OnCheckedChangeListener {
-        /**
-         * <p>Called when the checked radio button has changed. When the
-         * selection is cleared, checkedId is -1.</p>
-         *
-         * @param group the group in which the checked radio button has changed
-         * @param checkedId the unique identifier of the newly checked radio button
-         */
         void onCheckedChanged(ListeningCheckableGroup group, @IdRes int checkedId);
     }
 
@@ -267,18 +193,10 @@ public class ListeningCheckableGroup extends LinearLayout {
         }
     }
 
-    /**
-     * <p>A pass-through listener acts upon the events and dispatches them
-     * to another listener. This allows the table layout to set its own internal
-     * hierarchy change listener without preventing the user to setup his.</p>
-     */
     private class PassThroughHierarchyChangeListener implements
             ViewGroup.OnHierarchyChangeListener {
         private ViewGroup.OnHierarchyChangeListener mOnHierarchyChangeListener;
 
-        /**
-         * {@inheritDoc}
-         */
         public void onChildViewAdded(View parent, View child) {
             if (parent == ListeningCheckableGroup.this && child instanceof ListeningCheckable) {
                 ((ListeningCheckable) child).setOnCheckedChangeListener(mChildOnCheckedChangeListener);
@@ -288,9 +206,6 @@ public class ListeningCheckableGroup extends LinearLayout {
             }
         }
 
-        /**
-         * {@inheritDoc}
-         */
         public void onChildViewRemoved(View parent, View child) {
             if (parent == ListeningCheckableGroup.this && child instanceof ListeningCheckable) {
                 ((ListeningCheckable) child).setOnCheckedChangeListener(null);
