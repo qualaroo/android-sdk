40efda1731132a2a91f178f0d321b1e7502f59d3
Ajaypp242 <ajay@proprofs.com>
2023-01-31 17:03:07 +0530

    update

--- sample/src/main/java/com/qualaroo/sample/MainActivity.kt
+++ sample/src/main/java/com/qualaroo/sample/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.main_hello_button).setOnClickListener {
-            Qualaroo.getInstance().showSurvey("emoji_thumb_test")
+            Qualaroo.getInstance().showSurvey("alias-name")
         }
     }
 }
